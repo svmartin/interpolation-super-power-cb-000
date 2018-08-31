34477c91baf5dbd5e7d2064cc0867a3cfcdbccf7
Sean Martin <sean.martin@gmail.com>
2018-08-31 14:59:35 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,9 +2,9 @@
 def display_rainbow(colors)
   message = <<~EOF
   "#{colors[0][0].upcase}: #{colors[0]}, #{colors[1][0].upcase}: #{colors[1]},
-  #{colors[2][0].upcase}: #{colors[2]}, #{colors[3][0].upcase}: #{colors[3]},
-  #{colors[4][0].upcase}: #{colors[4]}, #{colors[5][0].upcase}: #{colors[5]},
-  #{colors[6][0].upcase}: #{colors[6]}
+    #{colors[2][0].upcase}: #{colors[2]}, #{colors[3][0].upcase}: #{colors[3]},
+    #{colors[4][0].upcase}: #{colors[4]}, #{colors[5][0].upcase}: #{colors[5]},
+    #{colors[6][0].upcase}: #{colors[6]}
   EOF
   puts message
 end
