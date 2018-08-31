b122e95a445c476daee3bf56c792b60dc87eeaec
Sean Martin <sean.martin@gmail.com>
2018-08-31 14:45:28 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  puts "#{}{colors[0][0].upcase}: #{colors[0]}"
+  puts "#{colors[0][0].upcase}: #{colors[0]}"
 end
 
 my_colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
