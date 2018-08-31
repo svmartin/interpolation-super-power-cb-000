b8c243010257ac13d8447d9ab7a248ca72997157
Sean Martin <sean.martin@gmail.com>
2018-08-31 14:39:47 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,8 +1,9 @@
 # Write your #display_rainbow method here
 def display_rainbow(colors)
-  colors.each do |color|
-    puts "#{color[0].upcase}: #{color}"
+  result = colors.map do |color|
+    return "#{color[0].upcase}: #{color}"
   end
+  result.join(", ")
 end
 
 my_colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
